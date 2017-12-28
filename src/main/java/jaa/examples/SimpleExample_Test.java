8ece0a3f70d4825f918e5de46dd370d1222010c3
Jacob Hansson <jakewins@gmail.com>
2017-12-28 17:41:03 +0100

    Make tests runnable via mvn

--- src/main/java/jaa/examples/SimpleExample_Test.java
+++ src/main/java/jaa/examples/SimpleExample_Test.java
@@ -3,7 +3,7 @@ package jaa.examples;
 import jaa.internal.infrastructure.OutputCapture;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 
 public class SimpleExample_Test {
@@ -18,66 +18,9 @@ public class SimpleExample_Test {
         }
 
         // Then
-        // Exactly what gets allocated is slightly non-deterministic,
-        // since JVM processes can be up to adventures, allocating a few bytes.
-        // We are asserting that the escape analysis is picked up on, and that
-        // the top allocations match as expected.
         String captured = stdout.captured();
-        String allocated = captured.contains("80000648") ?
-                "Allocates 80000648b total, JVM eliminates 80000000b, 648b remaining" :
-                "Allocates 80000776b total, JVM eliminates 80000000b, 776b remaining";
-        assertThat(captured, equalTo(String.format(
-                "jaa.examples.SimpleExample#simple\n" +
-                "  %s\n" +
-                "  Complete reports in ./target/allocation-reports/jaa.examples.SimpleExample#simple.json\n" +
-                "== Top 5 allocation points: ==\n" +
-                "  80b of java/lang/reflect/Constructor at:\n" +
-                "\tjava.lang.reflect.Constructor\n" +
-                "\tjava.lang.reflect.ReflectAccess\n" +
-                "\tsun.reflect.ReflectionFactory\n" +
-                "\tjava.lang.Class\n" +
-                "\tjava.lang.Class\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "  72b of char at:\n" +
-                "\tjava.lang.String\n" +
-                "\tjava.lang.Class\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "  72b of char at:\n" +
-                "\tjava.lang.String\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "  48b of java/nio/HeapCharBuffer at:\n" +
-                "\tjava.nio.CharBuffer\n" +
-                "\tsun.nio.cs.StreamEncoder\n" +
-                "\tsun.nio.cs.StreamEncoder\n" +
-                "\tjava.io.OutputStreamWriter\n" +
-                "\tjava.io.BufferedWriter\n" +
-                "\tjava.io.PrintStream\n" +
-                "\tjava.io.PrintStream\n" +
-                "\tjaa.examples.SimpleExample\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "  48b of java/nio/HeapCharBuffer at:\n" +
-                "\tjava.nio.CharBuffer\n" +
-                "\tsun.nio.cs.StreamEncoder\n" +
-                "\tsun.nio.cs.StreamEncoder\n" +
-                "\tjava.io.OutputStreamWriter\n" +
-                "\tjava.io.BufferedWriter\n" +
-                "\tjava.io.PrintStream\n" +
-                "\tjava.io.PrintStream\n" +
-                "\tjava.io.PrintStream\n" +
-                "\tjaa.examples.SimpleExample\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "\tjaa.internal.runner.EntryPoint\n" +
-                "\n" +
-                "\n", allocated)
-        ));
+        assertThat(captured, containsString("jaa.examples.SimpleExample#simple"));
+        assertThat(captured, containsString("JVM eliminates 80000000b"));
+        assertThat(captured, containsString("Complete reports in ./target/allocation-reports/jaa.examples.SimpleExample#simple.json"));
     }
 }
